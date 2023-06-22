f542f5bbfc7336023c21b8c149f9e3d1bf2051e5
RamyBadr1 <ramybadr054@gmail.com>
2023-06-22 19:59:47 +0300

    run_monty.c

--- run_monty.c
+++ run_monty.c
@@ -1,8 +1,72 @@
 #include "monty.h"
+#include <string.h>
+
+void free_tokens(void);
+unsigned int token_arr_len(void);
+int is_empty_line(char *line, char *delims);
+void (*get_op_func(char *opcode))(stack_t**, unsigned int);
+int run_monty(FILE *script_fd);
+
+/**
+ * free_tokens - Frees the global op_toks array of strings.
+ */
+void free_tokens(void)
+{
+	size_t i = 0;
+
+	if (op_toks == NULL)
+		return;
+
+	for (i = 0; op_toks[i]; i++)
+		free(op_toks[i]);
+
+	free(op_toks);
+}
+
+/**
+ * token_arr_len - Gets the length of current op_toks.
+ *
+ * Return: Length of current op_toks (as int).
+ */
+unsigned int token_arr_len(void)
+{
+	unsigned int toks_len = 0;
+
+	while (op_toks[toks_len])
+		toks_len++;
+	return (toks_len);
+}
+
+/**
+ * is_empty_line - Checks if a line read from getline only contains delimiters.
+ * @line: A pointer to the line.
+ * @delims: A string of delimiter characters.
+ *
+ * Return: If the line only contains delimiters - 1.
+ *         Otherwise - 0.
+ */
+int is_empty_line(char *line, char *delims)
+{
+	int i, j;
+
+	for (i = 0; line[i]; i++)
+	{
+		for (j = 0; delims[j]; j++)
+		{
+			if (line[i] == delims[j])
+				break;
+		}
+		if (delims[j] == '\0')
+			return (0);
+	}
+
+	return (1);
+}
 
 /**
  * get_op_func - Matches an opcode with its corresponding function.
  * @opcode: The opcode to match.
+ *
  * Return: A pointer to the corresponding function.
  */
 void (*get_op_func(char *opcode))(stack_t**, unsigned int)
@@ -34,16 +98,19 @@ void (*get_op_func(char *opcode))(stack_t**, unsigned int)
 		if (strcmp(opcode, op_funcs[i].opcode) == 0)
 			return (op_funcs[i].f);
 	}
+
 	return (NULL);
 }
 
 /**
  * run_monty - Primary function to execute a Monty bytecodes script.
  * @script_fd: File descriptor for an open Monty bytecodes script.
+ *
  * Return: EXIT_SUCCESS on success, respective error code on failure.
  */
 int run_monty(FILE *script_fd)
-{ stack_t *stack = NULL;
+{
+	stack_t *stack = NULL;
 	char *line = NULL;
 	size_t len = 0, exit_status = EXIT_SUCCESS;
 	unsigned int line_number = 0, prev_tok_len = 0;
@@ -51,8 +118,10 @@ int run_monty(FILE *script_fd)
 
 	if (init_stack(&stack) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
+
 	while (getline(&line, &len, script_fd) != -1)
-	{ line_number++;
+	{
+		line_number++;
 		op_toks = strtow(line, DELIMS);
 		if (op_toks == NULL)
 		{
@@ -60,15 +129,20 @@ int run_monty(FILE *script_fd)
 				continue;
 			free_stack(&stack);
 			return (malloc_error());
-		} else if (op_toks[0][0] == '#')
-		{ free_tokens();
-			continue; }
+		}
+		else if (op_toks[0][0] == '#') /* comment line */
+		{
+			free_tokens();
+			continue;
+		}
 		op_func = get_op_func(op_toks[0]);
 		if (op_func == NULL)
-		{ free_stack(&stack);
+		{
+			free_stack(&stack);
 			exit_status = unknown_op_error(op_toks[0], line_number);
 			free_tokens();
-			break; }
+			break;
+		}
 		prev_tok_len = token_arr_len();
 		op_func(&stack, line_number);
 		if (token_arr_len() != prev_tok_len)
@@ -78,12 +152,18 @@ int run_monty(FILE *script_fd)
 			else
 				exit_status = EXIT_FAILURE;
 			free_tokens();
-			break; }
-		free_tokens(); }
+			break;
+		}
+		free_tokens();
+	}
 	free_stack(&stack);
-	if (line && *line == 0)
-	{ free(line);
-		return (malloc_error()); }
-	free(line);
-	return (exit_status); }
 
+	if (line && *line == 0)
+	{
+		free(line);
+		return (malloc_error());
+	}
+
+	free(line);
+	return (exit_status);
+}
