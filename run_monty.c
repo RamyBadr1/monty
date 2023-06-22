9033a43eb41c59fea4daf3f51bd743483e4d690d
RamyBadr1 <ramybadr054@gmail.com>
2023-06-22 19:39:05 +0300

    get_op_func.c

--- run_monty.c
+++ run_monty.c
@@ -1,68 +1,6 @@
 #include "monty.h"
 
 /**
- * is_empty_line - Checks if a line read from getline only contains delimiters.
- * @line: A pointer to the line.
- * @delims: A string of delimiter characters.
- * Return: If the line only contains delimiters - 1.
- *         Otherwise - 0.
- */
-int is_empty_line(char *line, char *delims)
-{
-	int i, j;
-
-	for (i = 0; line[i]; i++)
-	{
-		for (j = 0; delims[j]; j++)
-		{
-			if (line[i] == delims[j])
-				break;
-		}
-		if (delims[j] == '\0')
-			return (0);
-	}
-	return (1);
-}
-
-/**
- * get_op_func - Matches an opcode with its corresponding function.
- * @opcode: The opcode to match.
- *
- * Return: A pointer to the corresponding function.
- */
-void (*get_op_func(char *opcode))(stack_t**, unsigned int)
-{
-	instruction_t op_funcs[] = {
-		{"push", monty_push},
-		{"pall", monty_pall},
-		{"pint", monty_pint},
-		{"pop", monty_pop},
-		{"swap", monty_swap},
-		{"add", monty_add},
-		{"nop", monty_nop},
-		{"sub", monty_sub},
-		{"div", monty_div},
-		{"mul", monty_mul},
-		{"mod", monty_mod},
-		{"pchar", monty_pchar},
-		{"pstr", monty_pstr},
-		{"rotl", monty_rotl},
-		{"rotr", monty_rotr},
-		{"stack", monty_stack},
-		{"queue", monty_queue},
-		{NULL, NULL}
-	};
-	int i;
-
-	for (i = 0; op_funcs[i].opcode; i++)
-	{
-		if (strcmp(opcode, op_funcs[i].opcode) == 0)
-			return (op_funcs[i].f);
-	}
-	return (NULL);
-}
-
-/**
  * run_monty - Primary function to execute a Monty bytecodes script.
  * @script_fd: File descriptor for an open Monty bytecodes script.
  * Return: EXIT_SUCCESS on success, respective error code on failure.
